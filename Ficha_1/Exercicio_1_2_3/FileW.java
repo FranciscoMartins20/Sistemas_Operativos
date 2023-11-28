b7fb57c34a81290b66d5f886cfbdf0518f8d9608
Francisco Martins <jfmartins200@gmail.com>
2023-11-28 10:10:28 +0000

    Correção de pastas

--- Ficha_1/Exercicio_1_2_3/FileW.java
+++ Ficha_1/Exercicio_1_2_3/FileW.java
@@ -1,4 +1,4 @@
-package Ficha_1;
+package Ficha_1.Exercicio_1_2_3;
 import java.io.*;
 import java.util.*;
 import java.nio.file.*;
--- Ficha_1/Exercicio_1_2_3/Hello.java
+++ Ficha_1/Exercicio_1_2_3/Hello.java
@@ -1,4 +1,4 @@
-package Ficha_1;
+package Ficha_1.Exercicio_1_2_3;
 
 public class Hello {
 public static void main(String[] args){
